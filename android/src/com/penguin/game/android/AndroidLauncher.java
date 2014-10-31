0f0b621f77b1a10d08f84aaa02429efbac2631d7
JoseRivas <jose.rivas.1997@gmail.com>
2014-10-31 13:46:09 -0400

    Physics : Gravity

---
+++ android/src/com/penguin/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.penguin.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.penguin.game.PenguinGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PenguinGame(), config);
+	}
+}
